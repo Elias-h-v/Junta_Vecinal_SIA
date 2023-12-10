146bd77c9c936c0e9d82272b62a4ef678850cb94
Elias Hormazabal <elias.hormazabal@alumnos.ucn.cl>
2023-12-10 04:00:00 -0300

    Se realizaron modificacion. El archivo anterior tenia errores de formato y ubicacion de carpetas

--- project_vecinos/urls.py
+++ project_vecinos/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for myproject project.
+"""project_vecinos URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,18 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.inicio, name='mi_vista_principal'),
-    path('perfil/', views.perfil, name='perfil'),
-    # Agrega más rutas según sea necesario
+    path('admin/', admin.site.urls),
+    path('vecinos_app', include('vecinos_app.urls')),
+
 ]
-
-
-
-
-
--- vecinos_app/views.py
+++ vecinos_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+
+# Create your views here.
 from django.http import HttpResponse
+
+def homeView(request):
+    return HttpResponse("HOLAAAAA")
 
 def inicio(request):
     return HttpResponse("¡Bienvenido a la página de inicio!")
